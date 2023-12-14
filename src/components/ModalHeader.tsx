363317be572f1f95d3bea107414161429aaad6d2
CorentinMaz <corentin.mazabrard@epitech.eu>
2023-12-14 16:13:42 +0100

    feat: extract all font

--- src/components/ModalHeader.tsx
+++ src/components/ModalHeader.tsx
@@ -7,26 +7,46 @@ const ModalHeader: React.FunctionComponent = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex w-full items-center justify-between bg-slate-500">
-      <div className="p-6">
-        <Button
-          onClick={() => navigate("/")}
-          className="font-bold"
-          type="text"
-        >
-          Le pavillon gourmand
-        </Button>
-      </div>
-      <div className="p-6">
-        <Button
-          onClick={() => {
-            console.log("test")
-          }}
-          className="font-bold"
-          type="text"
-        >
-          test
-        </Button>
+    <div className="absolute w-full">
+      <div className="flex justify-between">
+        <div className="p-6">
+          <Button
+            onClick={() => navigate("/")}
+            className="font-bold"
+            type="text"
+          >
+            Le pavillon gourmand
+          </Button>
+        </div>
+        <div className="p-6">
+          <Button
+            onClick={() => {
+              console.log("test")
+            }}
+            className="font-bold"
+            type="text"
+          >
+            Un lieu
+          </Button>
+          <Button
+            onClick={() => {
+              console.log("test")
+            }}
+            className="font-bold"
+            type="text"
+          >
+            Un moment
+          </Button>
+          <Button
+            onClick={() => {
+              console.log("test")
+            }}
+            className="font-bold"
+            type="text"
+          >
+            Nous trouver
+          </Button>
+        </div>
       </div>
     </div>
   );
