57f0cde4f19a66b6b3403cb1f7c29a387d45a841
thomas GRAND <thomas1.grand@epitech.eu>
2023-12-14 17:04:36 +0100

    push footer

--- src/views/FindUsView.tsx
+++ src/views/FindUsView.tsx
@@ -8,9 +8,7 @@ const FindUsView: React.FunctionComponent<RouteProps> = () => {
     return (
         <div className="flex h-screen flex-col justify-between">
             <ModalHeader />
-            <Button>
-                Find us Page
-            </Button>
+            <div className="w-full h-full bg-[url('../\src\assets\webp-images\4f6ad94d-47ef-4140-8384-32abd5685bdd_clos-des-sens-sept-2018-reportage-77.webp')] bg-contain">hello</div>
             <ModalFooter />
         </div>
     );
