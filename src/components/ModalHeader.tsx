e48fa0ad6e86631efdadf100a928c8744100e4c9
Mazabrard Corentin <corentin.mazabrard@epitech.eu>
2023-12-15 20:11:40 +0100

    feat: ajout page home

--- src/components/ModalHeader.tsx
+++ src/components/ModalHeader.tsx
@@ -1,51 +1,49 @@
 import React from "react";
 
-import { Button, Typography } from "antd";
-import { Link, useNavigate } from "react-router-dom";
+import { Typography } from "antd";
+import { Link } from "react-router-dom";
 import Logo from "../assets/src-eco-conception/logo.svg"
 
 const { Text } = Typography;
 
 const ModalHeader: React.FunctionComponent = () => {
-  const navigate = useNavigate();
   return (
-    <div className="absolute flex w-screen items-center justify-between p-6">
+    <div className="fixed flex w-screen items-center justify-between p-6">
       <div>
         <Link to={"/"} className="flex items-center justify-between">
           <img src={Logo} className="mr-3" />
-          <Text className="font-laBelleAurore sm:text-xl lg:text-4xl">
+          <Text className="font-laBelleAurore sm:text-xl lg:text-4xl text-white">
             Le pavillon gourmand
           </Text>
         </Link>
       </div>
       <div className="items-stretch p-1">
-        <Button
-          onClick={() => {
-            navigate('/place')
-          }}
-          className="font-jacquesFrancois sm:text-lg lg:text-2xl"
-          type="text"
-        >
-          Un lieu
-        </Button>
-        <Button
-          onClick={() => {
-            navigate('/moment')
-          }}
-          className="font-jacquesFrancois sm:text-lg lg:text-2xl"
-          type="text"
-        >
-          Un moment
-        </Button>
-        <Button
-          onClick={() => {
-            navigate('/find_us')
-          }}
-          className="font-jacquesFrancois sm:text-lg lg:text-2xl"
-          type="text"
-        >
-          Nous trouver
-        </Button>
+        <ul className="flex">
+          <li className="p-5">
+            <a
+              href="/place"
+              className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
+            >
+              Un lieu
+            </a>
+          </li>
+          <li className="p-5">
+            <a
+              href="/moment"
+              className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
+            >
+              Un moment
+            </a>
+          </li>
+          <li className="p-5">
+            <a
+              href="/find_us"
+              className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
+            >
+              Nous trouver
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,16 +1,40 @@
-import React from "react";
-import { Button } from "antd";
+import React, { useEffect, useState } from "react";
 import { RouteProps } from "react-router-dom";
 import ModalHeader from "../components/ModalHeader";
 import ModalFooter from "../components/ModalFooter";
+import { Typography } from 'antd';
+
+const { Text } = Typography;
 
 const HomeView: React.FunctionComponent<RouteProps> = () => {
     return (
         <div className="flex h-screen flex-col justify-between">
             <ModalHeader />
-            <Button>
-                Home Page
-            </Button>
+            <div className="flex flex-col">
+                <div className="w-full h-screen bg-[url('../\src\assets\webp-images\76ebb484-c1af-4e95-a46e-853f00d41124_reportage-portraits-cuisine-salle-46.webp')] bg-cover bg-no-repeat bg-top">
+                    <div className="absolute bottom-4 left-4 p-4">
+                        <Text className="text-white font-changaOne sm:text-xl lg:text-4xl text-5xl">
+                            Restaurant<br />
+                            gastronomique<br />
+                            végétal & lacustre<br />
+                            3* MICHELIN ANNECY<br />
+                        </Text>
+                    </div>
+                </div>
+                <div className="w-full h-screen flex">
+                    <div className="h-full w-1/2 bg-[#051528] flex justify-start items-center">
+                        <div className="flex flex-col p-4 w-2/3">
+                            <Text className="text-white font-changaOne sm:text-xl lg:text-4xl text-5xl">
+                                UN LIEU
+                            </Text>
+                            <Text className="text-[#F2F2F2] font-openSansLight sm:text-sm lg:text-lg text-2xl leading-10">
+                                Univers singulier, identitaire et façonné par l’homme. Essences de bois brulé, pierre de Savoie et cristal taillé, la salle, apaisante et épurée, accueille l’authentique transparence réconfort
+                            </Text>
+                        </div>
+                    </div>
+                    <div className="h-full w-1/2 bg-gray-500 flex items-center justify-center">MAP</div>
+                </div>
+            </div>
             <ModalFooter />
         </div>
     );
