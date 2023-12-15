9eba2d3da1016c75d0a2c1caa1c3cb132988b510
thomas GRAND <thomas1.grand@epitech.eu>
2023-12-15 14:06:55 +0100

    begin fin us page

--- src/views/FindUsView.tsx
+++ src/views/FindUsView.tsx
@@ -8,7 +8,13 @@ const FindUsView: React.FunctionComponent<RouteProps> = () => {
     return (
         <div className="flex h-screen flex-col justify-between">
             <ModalHeader />
-            <div className="w-full h-full bg-[url('../\src\assets\webp-images\4f6ad94d-47ef-4140-8384-32abd5685bdd_clos-des-sens-sept-2018-reportage-77.webp')] bg-contain">hello</div>
+            <div className="flex flex-col">
+                <div className="w-full h-[1080px] bg-[url('../\src\assets\webp-images\4f6ad94d-47ef-4140-8384-32abd5685bdd_clos-des-sens-sept-2018-reportage-77.webp')] bg-contain bg-no-repeat bg-top"></div>
+                <div className="w-full h-screen flex">
+                    <div className="h-full w-1/2 bg-gray-900"></div>
+                    <div className="h-full w-1/2 bg-gray-500 flex items-center justify-center">MAP</div>
+                </div>
+            </div>
             <ModalFooter />
         </div>
     );
