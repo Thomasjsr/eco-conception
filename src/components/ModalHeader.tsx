d0e500c03d724d68b68ad87a115d4f0746b806f8
Mazabrard Corentin <corentin.mazabrard@epitech.eu>
2023-12-21 09:09:56 +0100

    feat: add icon and nae page

--- src/components/ModalHeader.tsx
+++ src/components/ModalHeader.tsx
@@ -8,42 +8,44 @@ const { Text } = Typography;
 
 const ModalHeader: React.FunctionComponent = () => {
   return (
-    <div className="fixed flex w-screen items-center justify-between p-6">
-      <div>
-        <Link to={"/"} className="flex items-center justify-between">
-          <img src={Logo} className="mr-3" />
-          <Text className="font-laBelleAurore sm:text-xl lg:text-4xl text-white">
-            Le pavillon gourmand
-          </Text>
-        </Link>
-      </div>
-      <div className="items-stretch p-1">
-        <ul className="flex">
-          <li className="p-5">
-            <a
-              href="/place"
-              className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
-            >
-              Un lieu
-            </a>
-          </li>
-          <li className="p-5">
-            <a
-              href="/moment"
-              className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
-            >
-              Un moment
-            </a>
-          </li>
-          <li className="p-5">
-            <a
-              href="/find_us"
-              className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
-            >
-              Nous trouver
-            </a>
-          </li>
-        </ul>
+    <div>
+      <div className="fixed flex w-screen items-center justify-between p-6">
+        <div>
+          <Link to={"/"} className="flex items-center justify-between">
+            <img src={Logo} className="mr-3" />
+            <Text className="font-laBelleAurore sm:text-xl lg:text-4xl text-white">
+              Le pavillon gourmand
+            </Text>
+          </Link>
+        </div>
+        <div className="items-stretch p-1">
+          <ul className="flex">
+            <li className="p-5">
+              <a
+                href="/place"
+                className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
+              >
+                Un lieu
+              </a>
+            </li>
+            <li className="p-5">
+              <a
+                href="/moment"
+                className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
+              >
+                Un moment
+              </a>
+            </li>
+            <li className="p-5">
+              <a
+                href="/find_us"
+                className="font-jacquesFrancois sm:text-lg lg:text-2xl text-white hover:underline"
+              >
+                Nous trouver
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
