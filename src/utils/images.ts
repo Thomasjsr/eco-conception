f8a30d573024b16f217b6ac0d7f37693e9a9e5df
Mazabrard Corentin <corentin.mazabrard@epitech.eu>
2023-12-22 12:00:14 +0100

    feat: add all component image for home page

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,11 +1,40 @@
+// imagesHomeFirstCarousel
 import CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_38_2 from "../assets/webp-images/CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_38_2.webp";
 import reportage_portraits_cuisine_salle_46 from "../assets/webp-images/reportage_portraits_cuisine_salle_46.webp";
 import clos_des_sens_juin_2022_cortil_jardin_43 from "../assets/webp-images/clos_des_sens_juin_2022_cortil_jardin_43.webp";
 import cortil_septembre_2021_reportage_27 from "../assets/webp-images/cortil_septembre_2021_reportage_27.webp";
 
-export const imagesHomeFirstCarousel: string[] = [
+// imagesHomeSecondCarousel
+import clos_des_sens_juin_2022_salle_7 from "../assets/webp-images/clos_des_sens_juin_2022_salle_7.webp";
+import CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_17 from "../assets/webp-images/CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_17.webp";
+import CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_85 from "../assets/webp-images/CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_85.webp";
+
+// imagesHomeThirdCarousel
+import plat_de_haricots from "../assets/webp-images/plat_de_haricots.webp";
+import clos_des_sens_juillet_2023_plats_ecrevisse2 from "../assets/webp-images/clos_des_sens_juillet_2023_plats_ecrevisse2.webp";
+import clos_des_sens_juin_2022_plats_oxalis3 from "../assets/webp-images/clos_des_sens_juin_2022_plats_oxalis3.webp";
+
+const imagesHomeFirstCarousel: string[] = [
     reportage_portraits_cuisine_salle_46,
     CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_38_2,
     clos_des_sens_juin_2022_cortil_jardin_43,
     cortil_septembre_2021_reportage_27
 ];
+
+const imagesHomeSecondCarousel: string[] = [
+    clos_des_sens_juin_2022_salle_7,
+    CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_17,
+    CLOS_DES_SENS_MATTHIEU_CELLARD_SALLE_85,
+];
+
+const imagesHomeThirdCarousel: string[] = [
+    plat_de_haricots,
+    clos_des_sens_juillet_2023_plats_ecrevisse2,
+    clos_des_sens_juin_2022_plats_oxalis3,
+];
+
+export {
+    imagesHomeFirstCarousel,
+    imagesHomeSecondCarousel,
+    imagesHomeThirdCarousel
+}
