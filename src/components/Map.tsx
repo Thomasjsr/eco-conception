879cab6e7f1c24c121aa8175b1e55abf347c9c1e
GRAND Thomas <thomas1.grand@epitech.eu>
2023-12-27 00:18:10 +0100

    add responsive map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,13 +3,8 @@ import { Map, Marker, ZoomControl } from "pigeon-maps"
 
 const MapComponent: React.FunctionComponent = () => {
 
-    function getScreenHeight(): number {
-        const screenHeight = window.innerHeight;
-        return screenHeight;
-        }
-
   return (
-    <Map height={getScreenHeight()}  defaultCenter={[45.917844, 6.144424]} defaultZoom={15} metaWheelZoom={true}>
+    <Map defaultCenter={[45.917844, 6.144424]} defaultZoom={15} metaWheelZoom={true}>
         <Marker width={50} anchor={[45.917844, 6.144424]} />
         <ZoomControl />
     </Map>
